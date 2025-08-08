4d2c44119dae77f593dfb29566d9f1835aad04cf
Rakhi mishra <rakhimishra9572@gmail.com>
2025-08-08 21:01:00 +0530

    Add files via upload

---
+++ Abstract/src/Test.java
@@ -0,0 +1,48 @@
+
+
+//		//Abstract Class 
+//
+//		abstract class Vehicle {
+//
+//		}
+//
+//		public class Test {
+//
+//			public static void main(String[] args) {
+//				
+//			}
+//
+//		}
+	
+//Abstract Class 
+//
+//abstract class Vehicle {
+//	//Abstract Method
+//	abstract int getNoOfWheels();
+//	
+//	void color() {
+//		System.out.println("Black");
+//	}
+//	
+//	
+//}
+//
+//class Bike extends Vehicle {
+//
+//	int getNoOfWheels() {
+//		return 2;
+//	}
+//
+//}
+//
+//public class Test {
+//
+//	public static void main(String[] args) {
+//
+//		Bike b = new Bike();
+//		System.out.println(b.getNoOfWheels());
+//		b.color();
+//	}
+//
+//}
+//
